b2cfe9e3e46b4fdf7172161f40a926d0471f91b8
Harutyun <91012818+HarutyunTovmasyan19994@users.noreply.github.com>
2022-12-15 13:21:17 +0400

    Crocodile

---
+++ src/screens/Crocodile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import {Text, View} from "react-native"
+
+const Crocodile = ()=>{
+    return(
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+export default Crocodile
