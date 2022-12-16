f535200e4bf62f0f460be3c0814015aa1495a0a4
GorArestakesyan <103143478+GorArestakesyan@users.noreply.github.com>
2022-12-16 13:46:46 +0400

    check

--- src/services/DateTime/index.js
+++ src/services/DateTime/index.js
@@ -18,9 +18,9 @@ const DateTime = props => {
   const handleConfirm = date => {
     setOpen(false)
     setDate(date)
-    let monts = date.getMonth() + 1
-    let day = date.toString().slice(8, 10)
-    let year = date.toString().slice(11, 16)
+    // let monts = date.getMonth() + 1
+    // let day = date.toString().slice(8, 10)
+    // let year = date.toString().slice(11, 16)
     setter(prev => {
       return { day: day, month: monts, year: year }
     })
