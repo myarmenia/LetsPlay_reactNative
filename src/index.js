211080f3e94b4403b17c1afebe26241499435518
Vashik <vashik531@gmail.com>
2023-02-06 17:51:59 +0400

    scheme correcting

--- src/index.js
+++ src/index.js
@@ -8,9 +8,8 @@ const MyApp = () => {
   const { token } = useSelector(({ auth }) => auth)
   return (
     <>
-      {/*<StatusBar barStyle={'light-content'} backgroundColor={DARK_BLUE} />*/}
-      {/*{token ? <AppNavigator /> : <AuthNavigator />}*/}
-      <AppNavigator/>
+      <StatusBar barStyle={'light-content'} backgroundColor={DARK_BLUE} />
+      {token ? <AppNavigator /> : <AuthNavigator />}
     </>
   )
   {
