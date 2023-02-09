4e32c640f7d07f22847aa7a88465ee6b6b8fc951
Arsen <rustamyanarsen@gmail.com>
2023-02-09 12:41:02 +0400

    some changes for clen codes

--- src/screens/GameCreating/GameTicket/ticket.js
+++ src/screens/GameCreating/GameTicket/ticket.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import {Image, ScrollView, Text, View} from 'react-native'
-import Soccer from '@/assets/imgs/games/soccer.png'
+import { Image, ScrollView, Text, View } from 'react-native'
 import { RH, RW } from '@/theme/utils'
 import style from '@/screens/GameCreating/style'
-import ShareSvg from '@/assets/svgs/shareSvg'
-import Detail from '@/assets/imgs/detail.png'
 import moment from 'moment'
-import User from "@/assets/imgs/user/user";
-import {Players} from "@/assets/TestData";
+import User from '@/assets/imgs/user/user'
+import { Players } from '@/assets/TestData'
 
 function Ticket(props) {
   const { image, game, data } = props
@@ -24,26 +21,20 @@ function Ticket(props) {
           <Text style={style.ticketTextTwo}>{game.title}</Text>
         </View>
         <View style={style.ticketTextBlock}>
-          <Text style={style.ticketText}>
-            Дата и время игры:
-          </Text>
+          <Text style={style.ticketText}>Дата и время игры:</Text>
           <Text style={style.ticketTextTwo}>
-            {moment(data.gameDayDate).format('DD.MM.YY')} ,{' '}
-            {moment(data.gameDayTime).format('HH:mm')}
+            {/* {moment(data.gameDayDate).format('DD.MM.YY')} ,{' '}
+            {moment(data.gameDayTime).format('HH:mm')} */}
           </Text>
         </View>
         <View style={style.ticketTextBlock}>
-          <Text style={style.ticketText}>
-            Количество игроков:
-          </Text>
+          <Text style={style.ticketText}>Количество игроков:</Text>
           <Text style={style.ticketTextTwo}>
-           от {data.playerCountFrom} до {data.playerCountTo}
+            от {data.playerCountFrom} до {data.playerCountTo}
           </Text>
         </View>
         <View style={style.ticketTextBlock}>
-          <Text style={style.ticketText}>
-            Возраст игроков:
-          </Text>
+          <Text style={style.ticketText}>Возраст игроков:</Text>
           <Text style={style.ticketTextTwo}>
             {data.ageFrom}-{data.ageTo}
           </Text>
@@ -66,13 +57,17 @@ function Ticket(props) {
           <Text style={style.ticketText}>Стоимость входного билета на игру:</Text>
           <Text style={style.ticketTextTwo}>{data.priceValue ? data.priceValue : 0} руб.</Text>
         </View>
-        <View style={{...style.ticketTextBlock , marginBottom: RH(150)}}>
+        <View style={{ ...style.ticketTextBlock, marginBottom: RH(150) }}>
           <Text style={style.ticketText}>Организатор игры:</Text>
-          <View style={{width: RW(60) , marginLeft: RW(20)}}>
-            <User size={40} user={Players[6]} onPressItem={{
-              item: <User user={Players[6]} size={390}/>,
-              modalClose:false,
-            }}/>
+          <View style={{ width: RW(60), marginLeft: RW(20) }}>
+            <User
+              size={40}
+              user={Players[6]}
+              onPressItem={{
+                item: <User user={Players[6]} size={390} />,
+                modalClose: false,
+              }}
+            />
           </View>
         </View>
       </View>
