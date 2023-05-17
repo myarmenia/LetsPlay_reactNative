e2a0fce2aa993282990da686ad64d220fadfcede
Arsen <rustamyanarsen@gmail.com>
2023-05-17 18:39:57 +0400

    commit

--- src/screens/Team/TeamSchemes/components/assets/ArrowSvg.js
+++ src/screens/Team/TeamSchemes/components/assets/ArrowSvg.js
@@ -3,8 +3,8 @@ import Svg, { Path } from 'react-native-svg'
 
 function ArrowSvg(props) {
   return (
-    <Svg width={16} height={16} viewBox="0 0 17 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <Path d="M11 1l-9 9.5 9 9.5" stroke="#000000" strokeWidth={3} strokeLinecap="round" />
+    <Svg width={26} height={26} viewBox="0 0 17 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <Path d="M11 1l-9 9.5 9 9.5" stroke="#657AC5" strokeWidth={3} strokeLinecap="round" />
     </Svg>
   )
 }
